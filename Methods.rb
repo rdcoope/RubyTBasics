0912bd54d5de03241d4d4a0ac31140eddda02125
Richard <rdcoope@gmail.com>
2025-04-12 15:30:08 -0500

    Add a new file Methods and use it as a notepad The notepad usage of this file is mainly for what is being learned

---
+++ Methods.rb
@@ -0,0 +1,15 @@
+# Variables are a way to store data. 
+# Declaring a variable is easy . You use snake case for the variable name space = . Snake case is where the words are seperated by an _
+snake_case = 'Variable Declaration'
+#methods are similar to functions in other languages
+#Methods are declared like the following
+
+def variable_name (input_1,input_2)
+  a = 'Functions and variables go here'
+  a + ' ' + input_1 + ' ' + input_2
+
+end
+variable_name 'input1','input2'
+
+
+
