94bce96ffb76cad9bc9a70c73f27552fdd5931bc
Richard <rdcoope@gmail.com>
2025-04-12 12:45:55 -0500

    Follow along with the Instructions

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # RubyTBasics
 Learning Ruby
 
+Files in this project
+
+Basic testing
+    File1.rb -- Simple put statement
+    File2.rb -- Simple Put Statement
+
+TOP Ruby
+    BasicDataTypes.rb
---
+++ testProgram.rb
@@ -0,0 +1,25 @@
+movies = { :jaws => 1975,
+           :anchorman => 2004,
+           :man_of_steel => 2013,
+           :a_beautiful_mind => 2001,
+           :the_evil_dead => 1981 }
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
+
+puts movies
+
+
+
+
+mdates = [movies[:jaws],movies[:anchorman],movies[:man_of_steel],movies[:a_beautiful_mind]]
+puts mdates[0]
+puts mdates[1]
+puts mdates[2]
+puts mdates[3]
+puts mdates[4]
+
+puts mdates
