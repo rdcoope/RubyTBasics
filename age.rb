df47eaa9796b7c4481ac593490ea3d03197449f1
Richard <rdcoope@gmail.com>
2025-04-13 11:55:44 -0500

    Add more practice programs

---
+++ age.rb
@@ -0,0 +1,8 @@
+puts 'How old are you currently?'
+age = gets.chomp
+age = age.to_i
+
+puts 'in 10 years you will be '+ (age+10).to_s
+puts 'in 20 years you will be '+ (age+20).to_s
+puts 'in 30 years you will be '+ (age+30).to_s
+puts 'in 40 years you will be '+ (age+40).to_s
---
+++ name.rb
@@ -0,0 +1,6 @@
+puts 'What is your First name'
+first_name = gets.chomp
+puts 'What is your Last name'
+last_name = gets.chomp
+puts 'Hello ' + first_name + ' ' + last_name
+
