0b59015df6a671b9c6bd1d366d29aab836a4f019
Richard <rdcoope@gmail.com>
2025-04-24 15:04:49 -0500

    Add Methods from the Methods lesson in TOP

--- Methods.rb
+++ Methods.rb
@@ -11,5 +11,37 @@ def variable_name (input_1,input_2)
 end
 variable_name 'input1','input2'
 
+# Methods Lesson below here. 
+def numbers_method
+  def puts_squared(number)
+    puts number * number
+  end
+  def return_squared(number)
+    number * number
+  end
+  puts "This is the output of puts_squared"
+  puts_squared(20)
+
+  puts "This is the output of return_squared return_squared(20)"
+  return_squared(20)
+end
+def phrase_method
+  puts "Phrase method... "
+  phrase = ["be","to","not","or","be","to"]
+  puts "Phrase = " + phrase.to_s
+
+  puts "chaining methods (reverse, join, capitalize)"
+  puts phrase.reverse.join(" ").capitalize
+
+  puts phrase.reverse
+  puts phrase.join(" ")
+  puts phrase.each.capitalize
 
 
+  puts "End of phrase"
+end
+
+
+
+#numbers_method
+#phrase_method
